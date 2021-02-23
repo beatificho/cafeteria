da3b4e177ce9040adf1543b0fd64f8138de5b062
beatificho <beatificho@gmail.com>
2021-02-23 18:30:05 +0900

    graceful shutdown

--- point/src/main/java/cafeteriapoint/PointApplication.java
+++ point/src/main/java/cafeteriapoint/PointApplication.java
@@ -1,16 +1,29 @@
 package cafeteriapoint;
-import cafeteriapoint.config.kafka.KafkaProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import cafeteriapoint.config.kafka.KafkaProcessor;
+import cafeteriapoint.config.shutdown.TomcatGracefulShutdown;
 
 
 @SpringBootApplication
 @EnableBinding(KafkaProcessor.class)
 @EnableFeignClients
 public class PointApplication {
+	
+	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory(final TomcatGracefulShutdown gracefulShutdown) {
+	    TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+	    factory.addConnectorCustomizers(gracefulShutdown);
+	    return factory;
+	}
+	
     protected static ApplicationContext applicationContext;
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(PointApplication.class, args);
